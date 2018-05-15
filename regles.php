dcffc8e2c412856d043262bb08aee938833f638c
Louis <louis.chs@eduge.ch>
2018-05-15 15:41:25 +0200

    Ajout de la page règle

---
+++ regles.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Règles</title>
+</head>
+
+<body>
+    <div>
+        <h1>Règles du blindtest des synonymes</h1>
+		<ul>
+			<li>Le premier joueur choisit une des 3 chansons proposées</li>
+			<li>Il doit ensuite faire deviner la chanson SANS utiliser les mots présents dans la chanson</li>
+			<li>Le joueur qui devine le plus rapidement la chanson gagne des points</li>
+			<li>Vous pouvez arreter votre partie à tout moment</li>
+		</ul>
+	<a href="menu.php">Retour au menu</a>
+    </div>
+</body>
+
+</html>
