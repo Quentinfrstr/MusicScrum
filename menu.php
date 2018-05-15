2f7ddcd350aa0bfbb71667f6808fe3b4fdb05c0b
Louis <louis.chs@eduge.ch>
2018-05-15 14:01:04 +0200

    add menu

---
+++ menu.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Menu</title>
+</head>
+
+<body>
+    <div>
+        <h1>Bienvenue sur le blindtest des synonimes</h1>
+		<ul>
+			<li><a href="index.php">Jouer</a></li>
+			<li><a href="ajoutMusique.php">Ajouter des musiques</a></li>
+		</ul>
+    </div>
+</body>
+
+</html>
