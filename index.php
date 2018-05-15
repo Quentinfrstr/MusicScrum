f466ab50fa1aa9b0d677b55c88fdac99979c1882
Nicola <nicola.ggln@eduge.ch>
2018-05-15 13:54:05 +0200

    Fusion code Quentin ET Nicola

--- index.php
+++ index.php
@@ -6,36 +6,58 @@
     <title>Musical Blind-Test</title>
 </head>
 
-<body>
+<body onload="init()">
     <div id="choixMusiques">
-        <button id="btn1" onclick="afficheParoles('p1')">
-                Bouton 1
-            </button>
-        <button id="btn2" onclick="afficheParoles('p2')">
-                Bouton 2
-            </button>
-        <button id="btn3" onclick="afficheParoles('p3')">
-                Bouton 3
-            </button>
+        
     </div>
     <div id="affichageParoles">
         <label id="lblParoles">
                 
         </label>
     </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script type="text/javascript">
         // Intialisation des variables
 
 
         // Fonctions
-        
+
         // Affiches les paroles de la chanson séléctionnée par l'utilisateur. Fait Disparaître le div choix musique et apparaître le div de paroles.
         // Paramètres : paroles --> string : Sert à récupérer les paroles.
-        function afficheParoles(paroles) 
-        {
+        function afficheParoles(paroles) {
             choixMusiques.style.display = "none";
             affichageParoles.style.display = "block";
             lblParoles.innerHTML = paroles;
+        }
+
+        function randomMusic() {
+            $.ajax({
+                url: "./serveur.php",
+                type: "POST",
+                data: ({
+                    fonction: 'randomMusic'
+                }),
+                dataType: 'text',
+                success: function(reponse) {
+                    var ligne = reponse.split('|');
+                    for (var i = 0; i < ligne.length - 1; i++) {
+                        var infos = ligne[i].split(';');
+                        var btn = document.createElement('button');
+                        btn.innerHTML = infos[0];
+                        btn.paroles = infos[1];
+                        btn.id = btn + i;
+                        btn.onclick = function() {
+                            nomMusique = this.innerHTML;
+                            afficheParoles(this.paroles);
+                        }
+                        document.getElementById('choixMusiques').appendChild(btn);
+                    }
+                }
+            });
+        }
+        
+        function init(){
+            randomMusic();
         }
 
     </script>
--- serveur.php
+++ serveur.php
@@ -1,20 +1,20 @@
 <?php
 
 try{
-    $bdd = new PDO('mysql:host=;dbname=blindTest', 'root', '');
+    $bdd = new PDO('mysql:host=;dbname=musique', 'root', '');
 }catch(PDOException $e){
     echo 'Connexion échoué';
 }
 
 if($_POST['fonction'] == 'randomMusic'){
-    $infos = $bdd->query('SELECT titre, parole FROM blindtest');
+    $infos = $bdd->query('SELECT titre, paroles FROM chansons');
     $infos = $infos->fetchAll();
     
     $result = "";
     
     for($i = 0; $i < 3; $i++){
         $numChanson = rand(0, count($infos));
-        $result .= $infos[$numChanson]['titre'] . ';' .$infos[$numChanson]['parole']. '|';
+        $result .= $infos[$numChanson]['titre'] . ';' .$infos[$numChanson]['paroles']. '|';
     }
     
     echo $result;
