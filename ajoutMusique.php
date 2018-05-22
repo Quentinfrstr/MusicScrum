8942b527a01fe9bd8815f4477cd81aba59fc0050
Louis <louis.chs@eduge.ch>
2018-05-22 15:00:00 +0200

    Ajout CSS Ajoutmusique.php

--- ajoutMusique.php
+++ ajoutMusique.php
@@ -4,16 +4,22 @@
 <head>
     <meta charset="utf-8">
     <title>Ajouter une musique</title>
-</head>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+
+	<link rel="stylesheet" href="./addstylesheet.css"></head>
 
 <body>
-    <form method="POST" action="./verifAjoutMusique.php">
-        <input type="text" name="titre" placeholder="Entrez le titre de votre morceau" />
-        <input type="text" name="artiste" placeholder="Entrez le nom de l'artiste" />
-        <input type="text" name="refrain" placeholder="Entrez le refrain de votre morceau" />
-        <button type="submit">Ajouter</button>
-        <?php echo $_GET["succes"]; ?>
-    </form>
+<div class="form-group"><h1>Ajout de musiques</h1></div>
+<?php if(isset($_GET["succes"])){echo "<p id='succes'>s".$_GET["succes"]."</p>";} ?>
+    <div class="form-group" id="form"><form method="POST" action="./verifAjoutMusique.php">
+        <input class="form-control" type="text" name="titre" placeholder="Entrez le titre de votre morceau" />
+        <input class="form-control" type="text" name="artiste" placeholder="Entrez le nom de l'artiste" />
+		<textarea class="form-control" name="refrain" id="exampleFormControlTextarea1" placeholder="Entrez le refrain de votre morceau"rows="5"></textarea>
+        <button id="btn" type="submit" class="btn btn-primary btn-lg active">Ajouter</button>
+        
+    </form></div>
+	<div class="form-group"><a href="menu.php" id="btn" class="btn btn-primary btn-lg active">Retour au menu</a></div>
 </body>
 
 </html>
