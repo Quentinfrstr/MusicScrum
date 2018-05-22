34df4b5673285d6e743e76c3cae3dbd4c9c838b3
Louis <louis.chs@eduge.ch>
2018-05-22 15:16:07 +0200

    Ajout e l'auteur sur les boutons, début du CSS

--- index.php
+++ index.php
@@ -4,7 +4,8 @@
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>Musical Blind-Test</title>
-
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+	<link rel="stylesheet" href="./mainstylesheet.css">
 </head>
 
 <body onload="init()">
@@ -21,6 +22,9 @@
             
         </label>
     </div>
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
     <script type="text/javascript">
         // Intialisation des variables
@@ -55,9 +59,10 @@
                     for (var i = 0; i < ligne.length - 1; i++) {
                         var infos = ligne[i].split(';');
                         var btn = document.createElement('button');
-                        btn.innerHTML = infos[0];
+                        btn.innerHTML = infos[0] + " - " + infos[2];
                         btn.paroles = infos[1];
-                        btn.id = btn + i;
+                        btn.id = "btn";
+						btn.className = "btn btn-primary btn-lg active";
                         btn.onclick = function() {
                             nomMusique = this.innerHTML;
                             afficheParoles(this.paroles);
--- serveur.php
+++ serveur.php
@@ -7,14 +7,14 @@ try{
 }
 
 if($_POST['fonction'] == 'randomMusic'){
-    $infos = $bdd->query('SELECT titre, paroles FROM chansons');
+    $infos = $bdd->query('SELECT titre, paroles, auteur FROM chansons');
     $infos = $infos->fetchAll();
     
     $result = "";
     
     for($i = 0; $i < 3; $i++){
         $numChanson = rand(0, count($infos));
-        $result .= $infos[$numChanson]['titre'] . ';' . $infos[$numChanson]['paroles']. '|';
+        $result .= $infos[$numChanson]['titre'] . ';' . $infos[$numChanson]['paroles'] . ';' . $infos[$numChanson]['auteur'] . '|' ;
     }
     
     echo utf8_encode($result);
